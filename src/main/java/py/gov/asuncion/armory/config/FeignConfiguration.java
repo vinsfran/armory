ff4d750d8c28cf7434c82d777263df7a9259c863
Vicente Insfran <vinsfran@gmail.com>
2018-07-27 10:50:13 -0400

    Initial application generated by JHipster-5.1.0

---
+++ src/main/java/py/gov/asuncion/armory/config/FeignConfiguration.java
@@ -0,0 +1,18 @@
+package py.gov.asuncion.armory.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "py.gov.asuncion.armory")
+public class FeignConfiguration {
+
+    /**
+     * Set the Feign specific log level to log client REST requests
+     */
+    @Bean
+    feign.Logger.Level feignLoggerLevel() {
+        return feign.Logger.Level.BASIC;
+    }
+}
